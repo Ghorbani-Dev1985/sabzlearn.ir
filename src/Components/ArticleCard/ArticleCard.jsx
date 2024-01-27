8dda062da6004524b81490dda9523e90de41cb0c
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-01-27 13:30:32 +0330

    feat: develop LastArticle component

---
+++ src/Components/ArticleCard/ArticleCard.jsx
@@ -0,0 +1,29 @@
+import { QueryBuilder, Star } from "@mui/icons-material";
+
+import React from "react";
+import TomanGreen from "../../assets/Images/svgs/toman-green.svg";
+import { Link } from "react-router-dom";
+
+function ArticleCard({
+  to,
+  src,
+  isOffer,
+  offerPercent,
+  category,
+  title,
+  description,
+  teacherLink,
+  teacherName,
+  time,
+  studentCount,
+  offerPrice,
+  price,
+}) {
+  return (
+     <div className="flex flex-col overflow-hidden bg-white dark:bg-gray-800 shadow-light dark:shadow-none dark:border border-gray-700 rounded-2xl">
+
+     </div>
+  );
+}
+
+export default ArticleCard;
---
+++ src/Components/LastArticle/LastArticle.jsx
@@ -0,0 +1,141 @@
+import React from 'react'
+import SectionTitle from '../SectionTitle/SectionTitle'
+import { ChevronLeftOutlined, ChevronRightOutlined } from '@mui/icons-material'
+import Slider from '../../common/Slider/Slider'
+import { SwiperSlide } from 'swiper/react'
+import CourseCard from '../CourseCard/CourseCard'
+
+const newCourses = [
+  {
+      id: 1,
+      src: './src/assets/Images/Courses/Course-thumbnail-Dashboard2-1-768x432.webp',
+      isOffer : true,
+      offerPercent : 60,
+      category :  [{
+              id: 1,
+              categoryTitle: 'فرانت اند',
+              categoryLink : ''
+      }],
+      title: 'پیاده سازی داشبورد های حرفه ای با CSS و JS',
+      description: 'این دوره فرصتی فوق‌العاده برای ارتقاء مهارت‌های فرانت اند شماست. با یادگیری از جدیدترین تکنیک‌ها، انیمیشن‌ها ، ابزارهای پیشرفته',
+      teacherName: 'مهرشاد براتی',
+      time: '07:08',
+      studentCount : 282,
+      offerPrice: 620000,
+      price: 248000
+  },
+  {
+      id: 2,
+      src: './src/assets/Images/Courses/ezgif.com-jpg-to-webp-converted-33-1-768x432.webp',
+      isOffer : true,
+      offerPercent : 60,
+      category : [{
+          id: 1,
+          categoryTitle: ' ارتقای مهارت ها',
+          categoryLink : ''
+  }],
+      title: 'Clean Code برای برنامه نویسان JS',
+      description: ' فرق بین یه برنامه‌نویس تازه‌کار و حرفه‌ای نوع دیدگاه و طرز کدنویسیشونه. برنامه‌نویس حرفه‌ای کدی رو می‌نویسه که تست‌پذیر باشه ',
+      teacherName: ' محمدامین سعیدی راد',
+      time: '02:18',
+      studentCount : 232,
+      offerPrice: 1100000,
+      price: 440000
+  },
+  {
+      id: 3,
+      src: './src/assets/Images/Courses/ezgif.com-jpg-to-webp-converted-22-1-768x432.webp',
+      isOffer : true,
+      offerPercent : 60,
+      category : [{
+          id: 1,
+          categoryTitle: ' بک اند ',
+          categoryLink : ''
+  }],
+      title: 'آموزش حرفه ای NodeJS بدون پیش نیاز',
+      description: ' الان و امروز کمتر کسی هست که از بازار کار داغ نود جی اس با خبر نباشه و یکی از ',
+      teacherName: ' محمدامین سعیدی راد',
+      time: '61:45',
+      studentCount : 432,
+      offerPrice: 4350000,
+      price: 1740000
+  },
+  {
+      id: 4,
+      src: './src/assets/Images/Courses/IMAGE-1402-09-30-20_33_36-1-768x432.webp',
+      isOffer : true,
+      offerPercent : 60,
+      category : [{
+          id: 1,
+          categoryTitle: ' بک اند ',
+          categoryLink : ''
+  },
+  {
+      id: 2,
+      categoryTitle: ' پی اچ پی ',
+      categoryLink : ''
+}
+],
+      title: ' دوره پروژه محور لایووایر (Livewire Master)',
+      description: ' دوره لایووایر مستر (Livewire Master) یک دوره‌ی تمام عیار برای فول‌استک فریمورکِ لایووایر است که هدف آن آموزش قدم به ',
+      teacherName: '  آرمین هوشمند ',
+      time: '00:21',
+      studentCount : 24,
+      offerPrice: 699000,
+      price: 279600
+  },
+  {
+    id: 5,
+    src: './src/assets/Images/Courses/IMAGE-1402-09-30-20_33_36-1-768x432.webp',
+    isOffer : true,
+    offerPercent : 60,
+    category : [{
+        id: 1,
+        categoryTitle: ' بک اند ',
+        categoryLink : ''
+},
+{
+    id: 2,
+    categoryTitle: ' پی اچ پی ',
+    categoryLink : ''
+}
+],
+    title: ' دوره پروژه محور لایووایر (Livewire Master)',
+    description: ' دوره لایووایر مستر (Livewire Master) یک دوره‌ی تمام عیار برای فول‌استک فریمورکِ لایووایر است که هدف آن آموزش قدم به ',
+    teacherName: '  آرمین هوشمند ',
+    time: '00:21',
+    studentCount : 24,
+    offerPrice: 699000,
+    price: 279600
+}
+
+]
+
+function LastArticle() {
+  return (
+    // LastArticle Component
+    <section className='mt-25 relative'>
+       <SectionTitle squareColor="bg-pink-500 dark:bg-rose-500" title=" جدیدترین دوره ها" subTitle=" یادگیری و رشد توسعه فردی " isLink={true} to="" linkText="مشاهده همه مقالات"/>      
+       {/* LastArticle */}
+       <Slider SwiperNextBtnID="#NewCoursesSwiperNextBtn" SwiperPrevBtnID="#NewCoursesSwiperPrevBtn" >
+         {
+         
+         newCourses.map(({id, src , isOffer , offerPercent , category , title , description , teacherName , time , studentCount , offerPrice , price}) => {
+           return(
+             <React.Fragment key={id}>
+                          <SwiperSlide>
+                        <CourseCard src={src} isOffer={isOffer} offerPercent={offerPercent} category={category} title={title} description={description} teacherName={teacherName} time={time} studentCount={studentCount} offerPrice={offerPrice} price={price}/>
+                          </SwiperSlide>
+                    </React.Fragment>
+                )
+              })
+        }
+       </Slider>
+<div class="dark:hidden hidden md:block w-[500px] h-[500px] lg:w-[630px] lg:h-[630px] bg-pink-500 opacity-20 blur-2xl rounded-full -z-10 absolute -right-[320px] lg:-right-[400px] top-0"></div> 
+    </section>
+  )
+}
+
+export default LastArticle
+
+
