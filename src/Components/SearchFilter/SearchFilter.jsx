04f54ac9cd8f6d948e13f812e883f71364dc3d02
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-01-30 14:40:05 +0330

    feat: develop SearchFilter component

---
+++ src/Components/SearchFilter/SearchFilter.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function SearchFilter({children}) {
+  return (
+    <aside className='sticky top-36 space-y-5'>
+    {/* Search Box */}
+    <form className='space-y-5'>
+     <div className='h-17 shadow-light dark:shadow-none bg-white dark:bg-gray-800 dark:border border-gray-700 rounded-2xl'>
+        <div className='h-full flex-between text-slate-500 dark:text-gray-500'>
+         <input type='text' className='w-full bg-transparent dark:bg-transparent outline-none text-sm font-dana pr-7 border-none' placeholder='در بین دوره ها جستجو کنید'/>
+         <button type='submit' className='mr-4 ml-6'>
+             <Search className='size-7'/>
+         </button>
+        </div>
+     </div>
+     {children}
+     {/* Toggle Box */}
+     <div className='hidden sm:grid grid-cols-2 lg:grid-cols-1 gap-5'>
+        {/* Free Course */}
+         <div className='px-7 h-17 shadow-light dark:shadow-none bg-white dark:bg-gray-800 dark:border border-gray-700 rounded-2xl'>
+            <div className='flex-between h-full'>
+              <span className='text-sm text-zinc-700 dark:text-white select-none'>فقط دوره های رایگان</span>
+              <label className='relative cursor-pointer select-none'>
+                  <input type='checkbox' className='toggle__input absolute h-0 w-0 opacity-0'/>
+                  <span className='toggle__select'></span>
+              </label>
+            </div>
+         </div>
+        {/* Presell Course */}
+        <div className='px-7 h-17 shadow-light dark:shadow-none bg-white dark:bg-gray-800 dark:border border-gray-700 rounded-2xl'>
+          <div className='flex-between h-full'>
+            <span className='text-sm text-zinc-700 dark:text-white select-none'>در حال پیش فروش</span>
+            <label className='relative cursor-pointer select-none'>
+                  <input type='checkbox' className='toggle__input absolute h-0 w-0 opacity-0'/>
+                  <span className='toggle__select'></span>
+              </label>
+          </div>
+        </div>
+     </div>
+    </form>
+ </aside>
+  )
+}
+
+export default SearchFilter
