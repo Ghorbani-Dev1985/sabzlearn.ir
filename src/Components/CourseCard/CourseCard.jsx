27d5a5b261b7a2986d5e9c1b12f0a0dc9d6397f3
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-01-31 13:56:41 +0330

    feat: add skeleton to CourseCard

--- src/Components/CourseCard/CourseCard.jsx
+++ src/Components/CourseCard/CourseCard.jsx
@@ -1,6 +1,6 @@
 import { QueryBuilder, Star } from "@mui/icons-material";
 
-import React from "react";
+import React, { useState } from "react";
 import TomanGreen from "../../assets/Images/svgs/toman-green.svg";
 import { Link } from "react-router-dom";
 
@@ -19,8 +19,11 @@ function CourseCard({
   offerPrice,
   price,
 }) {
+  const [isImgLoaded , setIsImgLoaded] = useState(true)
+  console.log(isImgLoaded)
   return (
-    <div className="flex flex-col overflow-hidden bg-white dark:bg-gray-800 shadow-light dark:shadow-none dark:border dark:border-gray-700 rounded-2xl">
+    
+      isImgLoaded ? <div className="flex flex-col overflow-hidden bg-white dark:bg-gray-800 shadow-light dark:shadow-none dark:border dark:border-gray-700 rounded-2xl">
       {/* Course Img */}
       <div className="relative h-42">
         <Link to={to} className="w-full h-full block">
@@ -28,6 +31,7 @@ function CourseCard({
             src={src}
             className="block w-full h-full object-cover rounded-2xl"
             alt="ghorbani-dev.ir"
+           
           />
         </Link>
         {/* Offer Tag */}
@@ -133,7 +137,7 @@ function CourseCard({
               {
               price > 0 ?
              <>{price && price.toLocaleString()} <img src={TomanGreen} alt="ghorbani-dev.ir" className="size-4" /> </> :
-              <span class="font-Dana text-xl text-primary space-x-1.5">
+              <span className="font-Dana text-xl text-primary space-x-1.5">
               رایگان! </span>
               }
               
@@ -141,7 +145,29 @@ function CourseCard({
           </div>
         </div>
       </div>
+    </div> : <div role="status" className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
+    <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+        <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"/>
+            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+        </svg>
     </div>
+    <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+    <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+    <div className="flex items-center mt-4">
+       <svg className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+        </svg>
+        <div>
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
+            <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+    </div>
+    <span className="sr-only">Loading...</span>
+</div> 
+
   );
 }
 
