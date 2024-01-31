82cec40bb2bc49627f3d4ba81a29b8a0f1b2097f
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-01-31 11:12:45 +0330

    feat: develop mobile filter

--- src/Components/TopSort/TopSort.jsx
+++ src/Components/TopSort/TopSort.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { Close } from "@mui/icons-material";
+import { Divider } from "@mui/material";
 
 function TopSort({ children, BtnOne, BtnTwo, BtnThree, BtnFour }) {
+  const [showMobileFilter , setShowMobileFilter] = useState(false)
   return (
     <>
       {/* Sort In Mobile */}
-      <div className="flex sm:hidden items-center gap-3.5 mb-7">
+      <div onClick={() => setShowMobileFilter((prev) => !prev)} className="flex sm:hidden items-center gap-3.5 mb-7">
         <div className="flex items-center justify-center gap-x-2 w-1/2 py-2 px-4 text-sm text-zinc-700 dark:text-white bg-white dark:bg-gray-800 rounded-xl select-none">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -22,6 +25,10 @@ function TopSort({ children, BtnOne, BtnTwo, BtnThree, BtnFour }) {
           </svg>
           <span>فیلتر</span>
         </div>
+        {
+          showMobileFilter &&  <MobileFilter />
+        }
+       
         <div className="flex-center gap-x-2 w-1/2 py-2 px-4 text-sm text-zinc-700 dark:text-white bg-white dark:bg-gray-800 rounded-xl select-none">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -29,10 +36,10 @@ function TopSort({ children, BtnOne, BtnTwo, BtnThree, BtnFour }) {
             viewBox="0 0 25.811 17.879"
             className="size-6 text-gray-500"
             stroke="currentColor"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-miterlimit="10"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeMiterlimit="10"
           >
             <path d="M.556,16.379V0" transform="translate(4.349)" />
             <path d="M9.809,0,4.9,4.927,0,0" transform="translate(0 11.452)" />
@@ -54,10 +61,10 @@ function TopSort({ children, BtnOne, BtnTwo, BtnThree, BtnFour }) {
               viewBox="0 0 25.811 17.879"
               className="size-5"
               stroke="currentColor"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-miterlimit="10"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeMiterlimit="10"
             >
               <path d="M.556,16.379V0" transform="translate(4.349)" />
               <path
@@ -87,3 +94,61 @@ function TopSort({ children, BtnOne, BtnTwo, BtnThree, BtnFour }) {
 }
 
 export default TopSort;
+
+const MobileFilter = () => {
+  return (
+    <div className="filter">
+      <div className="filter__header">
+        <div className="filter__head-title">
+          <button onClick={() => setShowMobileFilter((prev) => !prev)} className="flex-center text-gray-500">
+            <Close className="size-6" />{" "}
+          </button>
+          <span className="font-Dana text-lg text-zinc-700 dark:text-white">
+            فیلتر
+          </span>
+        </div>
+        <button className="filter__clean-btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+            />
+          </svg>
+          حذف فیلتر ها
+        </button>
+      </div>
+      <div className="px-6">
+        {/* Free Course */}
+            <div className='flex-between h-full'>
+              <span className='text-sm text-zinc-700 dark:text-white select-none'>فقط دوره های رایگان</span>
+              <label className='relative cursor-pointer select-none'>
+                  <input type='checkbox' className='toggle__input absolute h-0 w-0 opacity-0'/>
+                  <span className='toggle__select'></span>
+              </label>
+            </div>
+          <Divider className="border-gray-200 dark:border-gray-700"/>
+        {/* Presell Course */}
+          <div className='flex-between h-full'>
+            <span className='text-sm text-zinc-700 dark:text-white select-none'>در حال پیش فروش</span>
+            <label className='relative cursor-pointer select-none'>
+                  <input type='checkbox' className='toggle__input absolute h-0 w-0 opacity-0'/>
+                  <span className='toggle__select'></span>
+              </label>
+          </div>
+      </div>
+      <div className="filter__footer">
+        <button className="filter__submit-btn">اعمال فیلتر ها</button>
+      </div>
+    </div>
+  );
+};
+
+export { MobileFilter };
