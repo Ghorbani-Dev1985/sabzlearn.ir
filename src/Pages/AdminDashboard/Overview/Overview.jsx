3562c157043edcc5bff36af089a3c9fcda301c38
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-02-14 16:09:59 +0330

    feat: create Infobox component

--- src/Pages/AdminDashboard/Overview/Overview.jsx
+++ src/Pages/AdminDashboard/Overview/Overview.jsx
@@ -9,6 +9,7 @@ import { useShowLoading } from "../../../Contexts/ShowLoadingContext";
 import SkeletonLoading from "../../../Components/SkeletonLoading/SkeletonLoading";
 import { DataGrid , faIR} from "@mui/x-data-grid";
 import { Alert } from "@mui/material";
+import InfosBox from "../../../Components/InfosBoxInDashboard/InfosBoxInDashboard";
 
 function Overview() {
   const { datas: infos } = useFetch("infos/p-admin", true);
@@ -163,20 +164,3 @@ function Overview() {
 
 export default Overview;
 
-const InfosBox = ({ title, count, icon, color }) => {
-  return (
-    <div
-      className={`${color} flex items-center gap-x-2.5 md:gap-x-4 flex-grow md:flex-grow-0 md:w-64 p-2 rounded-2xl`}
-    >
-      <div className="flex-center w-14 h-14 md:w-[68px] md:h-[68px] bg-white/20 rounded-2xl">
-        {icon}
-      </div>
-      <div className="flex flex-col gap-y-1.5 md:gap-y-2 text-white">
-        <span className="font-MorabbaBold text-xl">{title} </span>
-        <span className="font-DanaBold text-xl">{count}</span>
-      </div>
-    </div>
-  );
-};
-
-export { InfosBox };
