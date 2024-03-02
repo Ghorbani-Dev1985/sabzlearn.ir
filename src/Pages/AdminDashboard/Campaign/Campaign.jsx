b02adad443227902ee29341262bcd3b45cde9b9a
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-03-02 11:06:32 +0330

    Refactor: validation form with react hook form in Campaign component

--- src/Pages/AdminDashboard/Campaign/Campaign.jsx
+++ src/Pages/AdminDashboard/Campaign/Campaign.jsx
@@ -4,20 +4,44 @@ import { LocalOffer } from "@mui/icons-material";
 import Button from "../../../common/Form/Button";
 import usePost from "../../../Hooks/usePost";
 import toast from "react-hot-toast";
+import { useForm } from "react-hook-form";
+import ApiRequest from "../../../Services/Axios/Configs/Config";
+
 
 function Campaign() {
   const title = useTitle("   ایجاد کمپین - پنل کاربری");
   const [discountPercent , setDiscountPercent] = useState('')
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    isDirty,
+    isValid,
+    watch,
+    control,
+    setValue,
+    formState,
+  } = useForm(
+    {
+      mode: "all",
+    },
+    {
+      defaultValues: {
+        DiscountPercent: "",
+      },
+    }
+  );
       //New Function
-      const AddNewCampaignHandler = (event) => {
-        event.preventDefault()
-          if(discountPercent){
-              const addNew = usePost('offs/all' , JSON.stringify({discount : discountPercent}))
-              setDiscountPercent('')
-          }else{
-            toast.error('لطفا درصد تخفیف را وارد نمایید')
+      const AddNewCampaignHandler = (data) => {
+       
+        const ResponseResult = ApiRequest.post('offs/all' , {discount : data.DiscountPercent})
+        .then((response) => {
+          if(response.status === 200){
+            toast.success("فعالسازی کمپین با موفقیت انجام شد");
           }
+        })
+        reset()
       }
 
   return (
@@ -26,27 +50,41 @@ function Campaign() {
         <legend className="font-DanaBold text-zinc-700 dark:text-white text-xl my-6 mx-10 px-3">
           ایجاد کمپین  جدید
         </legend>
+        <form onSubmit={handleSubmit(AddNewCampaignHandler)}>
         <div className="flex flex-wrap justify-between gap-5 child:w-48p">
-          <div className="relative">
-          <input
-              type='text'
-              className= 'outline-none pl-9 sm:pl-12 bg-white'
-              placeholder='  درصد تخفیف*'
-              value={discountPercent}
-              onChange={(event) => setDiscountPercent(event.target.value)}
-              />
-          <LocalOffer className="left-3 sm:left-4" />
-          </div>
+        <div>
+              <div className="relative">
+                <input
+                  type="number"
+                  {...register("DiscountPercent", {
+                    required: "وارد کردن درصد تخفیف اجباری می باشد",
+                    maxLength: {
+                      value: 2,
+                      message: " لطفا حداکثر 2 کاراکتر وارد نمایید",
+                    },
+                  })}
+                  className={`${
+                    errors.DiscountPercent && "border border-rose-500"
+                  } outline-none pl-9 sm:pl-12`}
+                  placeholder="  کد تخفیف*"
+                />
+                <LocalOffer className="left-3 sm:left-4" />
+              </div>
+              <span className="block text-rose-500 text-sm my-2">
+                {errors.DiscountPercent && errors.DiscountPercent.message}
+              </span>
+            </div>
          </div>
         <div className="flex justify-end items-center">
           <Button
             btnType="submit"
-            className="button-md h-12 sm:button-lg rounded-xl button-primary my-5 sm:mt-4 disabled:bg-slate-500 disabled:opacity-50 disabled:cursor-text"
-            onClick={AddNewCampaignHandler}
+            className="button-md h-12 sm:button-lg rounded-xl select-none button-primary my-5 sm:mt-4 disabled:bg-slate-500 disabled:opacity-50 disabled:cursor-text"
+            disabled={!formState.isValid}
           >
             ایجاد  
           </Button>
         </div>
+        </form>
       </fieldset>
     </>
   );
